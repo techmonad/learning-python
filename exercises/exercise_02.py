512a140e8fa99c2b76680fb9be9fb815190d5d9a
Satendra Kumar <satendrakumar06@gmail.com>
2023-02-25 14:09:29 +0530

    Exercise update

--- exercises/exercise_02.py
+++ exercises/exercise_02.py
@@ -1,60 +1,44 @@
-#functional programming with Python
->>> data = [1,2,3,4]
->>> list(map(lambda x:x*x, data))
-[1, 4, 9, 16]
->>> list(filter(lambda x:x > 2, data))
-[3, 4]
->>> from functools import reduce
->>> reduce(lambda x,y: x+ y, data)
-10
->>>
-
-range
->>> [i for i in  range(1,10)]
-[1, 2, 3, 4, 5, 6, 7, 8, 9]
-
- reduce(lambda acc,elem: acc + elem , [[2,1],[2,3],[3,3],[4,5]], [])
-#faltten
->>> list(chain.from_iterable([[1,2], [3,4]]))
-[1, 2, 3, 4]
->>> from itertools import chain
->>> list(chain.from_iterable([[1,2], [3,4]]))
-[1, 2, 3, 4]
+from functools import reduce
+from itertools import chain
+from itertools import dropwhile
+from itertools import takewhile
 
 
-
-#drop while
->>> from itertools import dropwhile
->>> list(dropwhile(lambda x: x<5, [1,4,6,4,1]))
-[6, 4, 1]
-
->>> from itertools import takewhile
->>> list(takewhile(lambda x: x<5, [1,4,6,4,1]))
-[1, 4]
-		In [9]: def groupby(data):
-		   ...:     kv = {}
-		   ...:     for k,v in data:
-		   ...:         if k not in kv:
-		   ...:             kv[k]=[v]
-		   ...:         else:
-		   ...:             kv[k].append(v)
-		   ...:     return kv
-
-		In [10]: data = [('a', 1), ('b',2),('a',2)]
-
-		In [11]: groupby(data)
-		Out[11]: {'a': [1, 2], 'b': [2]}
-
-In [23]: from functools import reduce
-
-In [24]: reduce(lambda acc,elem: acc+[elem] if not elem in acc else acc , [2,1,2,3,3,3,4,5], [])
-Out[24]: [2, 1, 3, 4, 5]
-
-In [1]: from functools import reduce
-
-In [2]: reduce(lambda acc,elem: acc + elem , [[2,1],[2,3],[3,3],[4,5]], [])
-Out[2]: [2, 1, 2, 3, 3, 3, 4, 5]
+def square(data_list):
+    return list(map(lambda x: x * x, data_list))
 
 
-reduce(lambda acc,elem: acc + elem , [[2,1],[2,3],[3,3],[4,5]], [])
+if __name__ == '__main__':
+    data = [1, 2, 3, 4]
+    print(square(data))
+    # output => [1, 4, 9, 16]
+
+    filter_list = list(filter(lambda x: x > 2, data))
+    print(filter_list)
+    # output => [3, 4]
+
+    sum_list = reduce(lambda x, y: x + y, data)
+    print(sum_list)
+    # output => 10
+
+    range_list = [i for i in range(1, 10)]
+    print(range_list)
+    # output => [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    result = reduce(lambda acc, elem: acc + elem, [[2, 1], [2, 3], [3, 3], [4, 5]], [])
+    print(result)
+
+    # flatten
+    flatten_list = list(chain.from_iterable([[1, 2], [3, 4]]))
+    print(flatten_list)
+    # output => [1, 2, 3, 4]
+
+    # drop while
+    elem_list = list(dropwhile(lambda x: x < 5, [1, 4, 6, 4, 1]))
+    print(elem_list)
+    # output => [6, 4, 1]
+
+    elem_list2 = list(takewhile(lambda x: x < 5, [1, 4, 6, 4, 1]))
+    print(elem_list2)
+    # output => [1, 4]
 
--- exercises/exercise_03.py
+++ exercises/exercise_03.py
@@ -1,1 +1,7 @@
+import pandas as pd
 
+if __name__ == '__main__':
+    df = pd.read_csv("../data/Filetest.csv", sep="\t")
+    print(df.loc[1:10])
+    print(df.iloc[1:10])
+    print(df.shape)
